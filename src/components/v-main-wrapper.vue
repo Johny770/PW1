bf17c8c9437feee4ef5476155f0f974341657687
Johny <43562887+Johny770@users.noreply.github.com>
2020-04-13 11:32:43 +0300

    Add files via upload

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="v-main-wrapper">
+        <h1>{{title}}</h1>
+        <keep-alive>
+            <router-view></router-view>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-main-wrapper',
+        props: {},
+        data(){
+            return {
+                title: 'Magazin'
+            }
+        },
+        computed: {},
+        methods: {},
+        watch: {},
+        }
+
+</script>
+
+<style>
+    .v-main-wrapper{
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+</style>
---
+++ vue..config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    css: {
+        //fail de prelucrare scss
+        loader0ptions: {
+            sass: {
+                prependData: '@import "@/assets/styles.scss"'
+            }
+        }
+    }
+};
